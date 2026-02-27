6980ee8138e4272f0c76b20ac33e6b1718aec691
Parth <58473966+Parth4210@users.noreply.github.com>
2026-02-28 02:42:18 +0530

    Completed required exercises for foundation>loop-and-arrays

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let total = 0
+    for(let i = a;i<b+1;i++){
+        total+=i
+    }
+    return total
 };
 
 // Do not edit below this line
