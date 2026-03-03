0c07c3157fb1cb634134dc132c7909725036e589
Parth <58473966+Parth4210@users.noreply.github.com>
2026-03-03 14:06:19 +0530

    Completed questions for object_baiscs(12-16)

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr)=>total=total+curr,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr)=>total=total*curr,0)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  while(n>0){
+    fact=fact*i
+    n--
+  }
+  return fact
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((a,b)=>a.yearOfDeath-a.yearOfBirth>b.yearOfDeath-b.yearOfBirth?1:-1).reverse()[0]
 };
 
 // Do not edit below this line
